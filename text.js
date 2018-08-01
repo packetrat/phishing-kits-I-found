3bbf530e9b224a5844dc66b86965566b99e82fcd
Sean M Gallagher <sean.gallagher@arstechnica.com>
2018-08-01 10:24:47 -0400

    Apple phish supplemental Javascript

---
+++ text.js
@@ -0,0 +1,16 @@
+// http://applesecurityrisks.xyz/text.js //
+window.defaultNumber = '+1-800-969-5405';
+window.defaultText='Your |%model%| has been locked due to detected illegal activity! Immediately call Apple Support to unlock it!';
+window.text ={
+	'xhamster.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'perfectgirls.net':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'gotporn.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'anysex.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'sex.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'bravotube.net':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'mylust.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'manporn.xxx':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'anybunny.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'txxx.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!',
+	'findbestsolution.xyz':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support to unlock it!'
+};
